4538515240fcbf540cf8cf3a3ab1baff06b44052
datadave <605409+datadavev@users.noreply.github.com>
2024-05-17 14:03:56 -0400

    let requests properly encode params

--- isamples_export_client/export_client.py
+++ isamples_export_client/export_client.py
@@ -130,8 +130,9 @@ class ExportClient:
         if self._refresh_date is not None:
             query = self._query_with_timestamp()
 
-        create_url = f"{self._export_server_url}create?q={query}&export_format={self._format}"
-        response = self._rsession.get(create_url, headers=self._authentication_headers())
+        params = {"q":query, "export_format":self._format}
+        create_url = f"{self._export_server_url}create"
+        response = self._rsession.get(create_url, headers=self._authentication_headers(), params=params)
         if _is_expected_response_code(response):
             json = response.json()
             return json.get("uuid")
