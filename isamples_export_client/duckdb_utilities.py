997bf9299ecf8fa0d21c952b8fea3864df7f0523
Dave Vieglais <605409+datadavev@users.noreply.github.com>
2024-05-21 13:12:42 -0400

    Stac validation (#9)
    * Adjust stac output for validator
    
    * change time extent to tuple
    
    * Fix mypy grumbling, add TemporalExtent
    
    * Fix flake8 grumbling

--- isamples_export_client/duckdb_utilities.py
+++ isamples_export_client/duckdb_utilities.py
@@ -1,3 +1,4 @@
+import datetime
 import json
 from typing import Optional
 
@@ -14,15 +15,37 @@ class GeoFeaturesResult:
         return f"GeoFeaturesResult geo_json={self.geo_json}, bbox={self.bbox}"
 
 
+class TemporalExtent(tuple):
+
+    def __new__(self, t0: Optional[datetime.datetime], t1: Optional[datetime.datetime]):
+        return tuple.__new__(TemporalExtent, (t0, t1))
+
+
 def read_geo_features_from_jsonl(filename: str) -> Optional[GeoFeaturesResult]:
     con = duckdb.connect()
     con.install_extension("spatial")
     con.load_extension("spatial")
     con.read_json(filename, format="newline_delimited")
     location_prefix = "produced_by.sampling_site.sample_location."
-    q = f"select ST_Extent(envelope)as bb, ST_AsGEOJSON(envelope) as poly from (select ST_Envelope_Agg(ST_Point({location_prefix}longitude, {location_prefix}latitude)) as envelope from '{filename}' where {location_prefix}longitude is not null)"
+    q = ("select ST_Extent(envelope)as bb, ST_AsGEOJSON(envelope) as poly "
+         f"from (select ST_Envelope_Agg(ST_Point({location_prefix}longitude, {location_prefix}latitude)) as envelope "
+         f"from '{filename}' where {location_prefix}longitude is not null)")
     spatial_results = con.sql(q).fetchone()
     if spatial_results is not None:
         return GeoFeaturesResult(spatial_results[0], spatial_results[1])
     else:
         return None
+
+
+def get_temporal_extent_from_jsonl(filename: str) -> TemporalExtent:
+    con = duckdb.connect()
+    con.read_json(filename, format="newline_delimited")
+    q = f"SET TimeZone='UTC'; CREATE TABLE samples AS SELECT * FROM read_json('{filename}', format='newline_delimited');"
+    con.sql(q)
+    q = ("SELECT min(produced_by.result_time::TIMESTAMPTZ) as min_t, "
+         "max(produced_by.result_time::TIMESTAMPTZ) as max_t "
+         "from samples where produced_by.result_time is not null")
+    result = con.sql(q).fetchone()
+    if result is not None:
+        return TemporalExtent(result[0], result[1])
+    return TemporalExtent(None, None)
