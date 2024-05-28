5417913ee4b152529601ae4116cfc96e17b443b9
Danny Mandel <danny.mandel@gmail.com>
2024-05-28 14:57:11 -0700

    Add a test for writing manifest file

--- tests/test_export_client.py
+++ tests/test_export_client.py
@@ -1,4 +1,7 @@
 import datetime
+import json
+import os.path
+import shutil
 import uuid
 from datetime import timezone, timedelta
 
@@ -35,10 +38,17 @@ def solr_query():
 
 @pytest.fixture
 def export_client(solr_query: str) -> ExportClient:
-    return ExportClient(solr_query, "./test_data/", "123456", "http://foo", "jsonl", "title", "description")
+    test_data_dir = os.path.join(os.path.dirname(__file__), f"test_data/{uuid.uuid4()}")
+    if os.path.exists(test_data_dir):
+        shutil.rmtree(test_data_dir)
+    return ExportClient(solr_query, test_data_dir, "123456", "http://foo", "jsonl", "title", "description")
 
-def test_stac(geo_features_result: GeoFeaturesResult, temporal_extent: TemporalExtent, solr_query: str, export_client: ExportClient):
-    stac_file = export_client.write_stac_item(str(uuid.uuid4()), datetime.datetime.now(), geo_features_result, temporal_extent, solr_query, "123456.jsonl", "123456.jsonl_geo.parquet")
+@pytest.fixture
+def uuid_fixture() -> str:
+    return str(uuid.uuid4())
+
+def test_stac(geo_features_result: GeoFeaturesResult, temporal_extent: TemporalExtent, solr_query: str, export_client: ExportClient, uuid_fixture: str):
+    stac_file = export_client.write_stac_item(uuid_fixture, datetime.datetime.now(), geo_features_result, temporal_extent, solr_query, "123456.jsonl", "123456.jsonl_geo.parquet")
     stac = stac_validator.StacValidate(stac_file, extensions=True)
     result = stac.run()
     print(stac.message)
@@ -51,3 +61,10 @@ def test_stac_catalog(geo_features_result: GeoFeaturesResult, temporal_extent: T
     result = stac.run()
     print(stac.message)
     assert result is True
+
+
+def test_write_manifest(solr_query: str, export_client: ExportClient, uuid_fixture: str):
+    manifest_path = export_client.write_manifest(solr_query, uuid_fixture, datetime.datetime.now(), 100, export_client._destination_directory)
+    with open(manifest_path, "r") as file:
+        data = json.load(file)
+        assert len(data) > 0
